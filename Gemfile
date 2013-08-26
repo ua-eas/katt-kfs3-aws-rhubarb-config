c3c5638ae3504a926339d72ff5b4472913f6c769
Josh Shaloo <shaloo@email.arizona.edu>
2013-08-26 16:53:54 -0700

    Initial commit to capify

---
+++ Gemfile
@@ -0,0 +1,5 @@
+
+
+group :deploy do
+  gem 'capistrano', '~> 3.0.0.pre13'
+end
---
+++ config/deploy/prod.rb
@@ -0,0 +1,7 @@
+set :stage, :prod
+
+# Deploy to a50, and a51
+role :app, "kualiadm@uaz-kc-a50.mosaic.arizona.edu", "kualiadm@uaz-kc-a51.mosaic.arizona.edu"
+
+#deploy the latest tag
+set :branch, `git tag`.split("\n").last
