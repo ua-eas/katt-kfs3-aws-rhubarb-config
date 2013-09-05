f2bab912c8b875a4ab7f8a90f3fa806c7061a2ca
sskinner <sskinner@email.arizona.edu>
2013-09-05 13:12:12 -0700

    Added comments that detail how to troubleshoot a failed test from the resulting stdout statements/stacktrace.

--- spec/yaml_validation_spec.rb
+++ spec/yaml_validation_spec.rb
@@ -1,3 +1,13 @@
+# Internal: RSpec tests to verify that all YAML configuration files are
+#           compatible and valid relative to Ruby's core YAML parser. Note,
+#           this test recursively spiders down into the directory structure
+#           contained within '$PROJECT/email' and attempts to load any files
+#           that match '*.yaml'. If a configuration throws an error, this test
+#           prints the offending file path and name to stdout. Additionally,
+#           the stack trace Ruby produces will contain the line and column
+#           number of where the parse error originates in the above mentioned
+#           file.
+#
 require_relative './yaml_validator.rb'
 
 describe YamlValidator do
@@ -13,13 +23,14 @@ describe YamlValidator do
       # Parse each config, any thrown errors mean failure
       yaml_file_list.each do |yaml_file_path|
         begin
+          # Send the path/name for validation
           YamlValidator.validate(yaml_file_path)
         rescue Exception => e
+          # Report problem child file, and propegate the error
           puts "ERROR: Could not load '#{yaml_file_path}'"
           throw e
         end
       end
-
     end
 
 end
