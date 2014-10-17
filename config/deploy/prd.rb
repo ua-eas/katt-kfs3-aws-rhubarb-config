67481e99b7245e12debf8e4e303097a2477a1ed9
heather-lo <hlo@email.arizona.edu>
2014-10-17 13:54:50 -0700

    KATTS-2645 Fixing syntax for deploying to multiple hosts

--- config/deploy/prd.rb
+++ config/deploy/prd.rb
@@ -1,7 +1,7 @@
 set :stage, :prd
 
 # Deploy to a50, and a51
-role :app, "kualiadm@uaz-kc-a50.mosaic.arizona.edu", "kualiadm@uaz-kc-a51.mosaic.arizona.edu"
+role :app, %w{kualiadm@uaz-kc-a50.mosaic.arizona.edu kualiadm@uaz-kc-a51.mosaic.arizona.edu} 
 
 #deploy the latest tag
 set :branch, `git tag`.split("\n").last
