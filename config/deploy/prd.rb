437ceebdc69c6599da21a54d88a95e43f4e419cd
heather-lo <hlo@email.arizona.edu>
2015-06-23 17:03:20 -0700

    KATTS-3469 modifying git tag command to sort the tags

--- config/deploy/prd.rb
+++ config/deploy/prd.rb
@@ -3,5 +3,5 @@ set :stage, :prd
 # Deploy to a50, and a51
 role :app, %w{kualiadm@uaz-kc-a50.mosaic.arizona.edu kualiadm@uaz-kc-a51.mosaic.arizona.edu} 
 
-#deploy the latest tag
-set :branch, `git tag`.split("\n").last
+# deploy the latest tag using natural sorting because we are not using Git 2.x
+set :branch, `git tag | sort -V`.split("\n").last
--- config/deploy/trn.rb
+++ config/deploy/trn.rb
@@ -3,5 +3,5 @@ set :stage, :trn
 # Set the app server host and port
 role :app, %w{kualiadm@uaz-kf-a06.mosaic.arizona.edu}
 
-# Deploy from the latest tag
-set :branch, `git tag`.split("\n").last
+# Deploy from the latest tag using natural sorting because we are not using Git 2.x
+set :branch, `git tag | sort -V`.split("\n").last
